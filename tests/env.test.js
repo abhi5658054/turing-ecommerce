d9842f92ccc6d07b66ed15a6c762a742f9fc3b92
Abhishek Menon <abhishek.mv1995@gmail.com>
2019-02-28 02:09:18 +0530

    test: added test to check NODE_ENV variable

--- tests/env.test.js
+++ tests/env.test.js
@@ -4,4 +4,7 @@ describe('Test the configuration', () => {
     test('if the Environment variables are present', () => {
         expect(process.env.SECRET_KEY).toBeTruthy();
     });
+    test('if NODE_ENV is present', () => {
+        expect(process.env.NODE_ENV).toBeTruthy();
+    })
 })
