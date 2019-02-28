b6e2a013d8ea492e40f44f989dcbbf75743139c8
Abhishek Menon <abhishek.mv1995@gmail.com>
2019-02-28 18:40:28 +0530

    feat: changed css to scss, added bulma

--- server/app.js
+++ server/app.js
@@ -1,11 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
+import path from "path";
 
 const app = express();
+const isDist = __dirname.includes("/dist/") ? true : false;
+app.use(express.static(path.join(__dirname, isDist ? "../" : "../dist/")));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-    res.send("HELLO WORLD!!")
-})
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, isDist ? "../index.html" : "../dist/index.html")
+  );
+});
 
 module.exports = app;
